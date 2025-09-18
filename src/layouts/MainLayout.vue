4e611ad6918091bbbe49f3f06dcb2db0078964a9
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-09-18 16:03:11 +0800

    added: loading indicator when fetching group details

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import { LoaderCircle } from 'lucide-vue-next';
 import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
     <div class="p-4">
-        <RouterView>
+        <RouterView v-slot="{ Component }">
+            <template v-if="Component">
+                <Suspense timeout="0">
+                    <component :is="Component"></component>
+                    <template #fallback>
+                        <div class="h-screen grid place-content-center">
+                            <LoaderCircle class="animate-spin size-4" />
+                        </div>
+                    </template>
+                </Suspense>
+            </template>
         </RouterView>
     </div>
 </template>
