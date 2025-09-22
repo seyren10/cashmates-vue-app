831bfcb54f6839f665ed5dd46e1b38b1ba0b0a3a
seyren10 <87021985+seyren10@users.noreply.github.com>
2025-09-22 21:28:29 +0800

    added: icon on savings-goal header

--- src/pages/groups/savings-goal/SavingsGoalHeader.vue
+++ src/pages/groups/savings-goal/SavingsGoalHeader.vue
@@ -3,6 +3,7 @@ import AppHeader from '@/components/app/AppHeader.vue'
 import { getSavingsGoalQueryOptions } from '@/features/savings-goal/query-options';
 import type { SavingsGoalId } from '@/features/savings-goal/type';
 import { useQuery } from '@tanstack/vue-query';
+import { Goal } from 'lucide-vue-next';
 import { computed } from 'vue';
 
 const { savingsGoalId } = defineProps<{
@@ -17,7 +18,10 @@ const savingsGoal = computed(() => data.value!)
     <AppHeader>
         <div class="grow capitalize">
             <h1 class="font-bold">{{ savingsGoal.name }}</h1>
-            <p class="text-xs text-muted-foreground">Savings</p>
+            <div class="text-xs text-muted-foreground flex items-center gap-1">
+                <Goal class="size-4" />
+                <p>Goal</p>
+            </div>
         </div>
     </AppHeader>
 </template>
